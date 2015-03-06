66c81d26a6f8e89d375b9132813a2a5f595ed04d
Steven Roper <steven.m.roper@gmail.com>
2015-03-05 20:35:25 -0700

    Finishing step 2 and configuring routing.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var app = angular.module('rtfmApp', []);
+var app = angular.module('rtfmApp', ['firebase', 'ngRoute']);
+
+app.config(function($routeProvider) {
+
+	$routeProvider
+	.when('/login', {
+
+	})
+	.when('/threads', {
+
+	})
+	.when('/threads/:threadId', {
+
+	})
+	.otherwise({
+		redirectTo: '/login'
+	});
+
+});
